1697703c17dbfc38c465512df74ec754f0969631
Micah Shiels <32394804+MicahGH@users.noreply.github.com>
2026-08-12 17:02:26 +0200

    Change JSONB to JSON

--- models/postgres/malformed_transaction.py
+++ models/postgres/malformed_transaction.py
@@ -1,7 +1,7 @@
 from datetime import UTC, datetime
 from typing import Any
 
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import JSON
 from sqlmodel import Field
 
 from models.postgres.base_sql_model import BaseSQLModel
@@ -12,8 +12,8 @@ class MalformedTransaction(BaseSQLModel, table=True):
 
     __tablename__ = "malformed_transaction"  # type: ignore[reportAssignmentType]
 
-    raw_data: dict[str, Any] = Field(sa_type=JSONB)
-    errors: list[dict[str, Any]] = Field(sa_type=JSONB)
+    raw_data: dict[str, Any] = Field(sa_type=JSON)
+    errors: list[dict[str, Any]] = Field(sa_type=JSON)
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(UTC),
     )
